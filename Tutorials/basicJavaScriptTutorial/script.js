03c6b2e9e137296204b73a953112acfb59f8e52e
mccrackudel <72521821+mccrackudel@users.noreply.github.com>
2021-09-16 21:01:53 -0400

    javascript edits

--- Tutorials/basicJavaScriptTutorial/script.js
+++ Tutorials/basicJavaScriptTutorial/script.js
@@ -1,1 +1,23 @@
 alert("hello!");
+var num = prompt('Enter a number between 1 and 5');
+if(num === 1){
+    alert("You entered 1");
+}
+else if(num === 2){
+    alert("You entered 2");
+}
+else if(num === 3){
+    alert("You entered 3");
+}
+else if(num === 4){
+    alert("You entered 4");
+}
+else if(num === 4){
+    alert("You entered 4");
+}
+else if(num === 5){
+    alert("You entered 5");
+}
+else{
+    alert("You entered something other than a number between 1 and 5")
+}
