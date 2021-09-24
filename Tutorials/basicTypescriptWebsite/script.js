687846aa901a6d172c751194b57805c8273d95c1
mccrackudel <72521821+mccrackudel@users.noreply.github.com>
2021-09-23 22:25:46 -0400

    edits

---
+++ Tutorials/basicTypescriptWebsite/script.js
@@ -0,0 +1,9 @@
+//alert("hello in typescript");
+var anyVar = "any string";
+var numVar = 22;
+var strVar = "Andrew McCracken";
+var boolVal = false;
+function greeter(person) {
+    return "hello, " + person;
+}
+document.body.textContent = greeter(strVar);
