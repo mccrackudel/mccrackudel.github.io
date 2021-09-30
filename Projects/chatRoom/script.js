2d42d35e698dd22d5d7a8aecbb75859cdeef798b
mccrackudel <72521821+mccrackudel@users.noreply.github.com>
2021-09-30 12:26:25 -0400

    edits

--- Projects/chatRoom/script.js
+++ Projects/chatRoom/script.js
@@ -1,7 +1,7 @@
 const username = prompt("Enter Name:");
 
-import { initializeApp } from 'firebase/app';
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
+import * as fb from  "https://www.gstatic.com/firebasejs/9.0.1/firebase-database.js";
 const firebaseConfig = {
     apiKey: "AIzaSyAe-7uubNezkH5Zw_b2qdQemAjJ3z5o5DA",
     authDomain: "cisc275chatroom.firebaseapp.com",
