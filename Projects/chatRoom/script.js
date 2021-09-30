c8bcd7a29cc321b07a1f673ae47030b8ad9a1d35
mccrackudel <72521821+mccrackudel@users.noreply.github.com>
2021-09-30 10:08:39 -0400

    edits

---
+++ Projects/chatRoom/script.js
@@ -0,0 +1,40 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAe-7uubNezkH5Zw_b2qdQemAjJ3z5o5DA",
+    authDomain: "cisc275chatroom.firebaseapp.com",
+    databaseURL: "https://cisc275chatroom-default-rtdb.firebaseio.com",
+    projectId: "cisc275chatroom",
+    storageBucket: "cisc275chatroom.appspot.com",
+    messagingSenderId: "1019765562309",
+    appId: "1:1019765562309:web:a9a937268e9b48972e49ac",
+    measurementId: "G-D3N129118Y"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+  const database = firebase.database;
+
+  const username = prompt("Enter Name:");
+  document.getElementById("submitButton").addEventListener("submit", sendMessage);
+
+  function sendMessage(e) {
+    e.preventDefault();
+
+    const timestamp = Date.now();
+    const messageInput = document.getElementById("textBox");
+    const message = messageInput.value;
+  
+    messageInput.value = "";
+  
+    document
+      .getElementById("chatBox")
+      .scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+  
+    // create db collection and send in the data
+    database.ref("messages/" + timestamp).set({
+      username,
+      message,
+    });
+  }
