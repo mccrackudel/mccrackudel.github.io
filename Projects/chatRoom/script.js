8f51c98ad7b7bdda97774fdaa044d0c183be9143
mccrackudel <72521821+mccrackudel@users.noreply.github.com>
2021-09-30 13:43:19 -0400

    edits

--- Projects/chatRoom/script.js
+++ Projects/chatRoom/script.js
@@ -1,27 +1,24 @@
 const username = prompt("Enter Name:");
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js";
-//import * as fb from  "https://www.gstatic.com/firebasejs/9.0.1/firebase-database.js";
+import * as fb from  "https://www.gstatic.com/firebasejs/9.0.1/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAe-7uubNezkH5Zw_b2qdQemAjJ3z5o5DA",
-    authDomain: "cisc275chatroom.firebaseapp.com",
-    databaseURL: "https://cisc275chatroom-default-rtdb.firebaseio.com",
-    projectId: "cisc275chatroom",
-    storageBucket: "cisc275chatroom.appspot.com",
-    messagingSenderId: "1019765562309",
-    appId: "1:1019765562309:web:a9a937268e9b48972e49ac",
-    measurementId: "G-D3N129118Y"
-  };
-  
-  // Initialize Firebase
-  const database = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAJFmJSQEeZXx0qequ9i6_3JEgs66wsDX0",
+  authDomain: "chatroom-fed7d.firebaseapp.com",
+  databaseURL: "https://chatroom-fed7d-default-rtdb.firebaseio.com",
+  projectId: "chatroom-fed7d",
+  storageBucket: "chatroom-fed7d.appspot.com",
+  messagingSenderId: "880533252950",
+  appId: "1:880533252950:web:7f4dacbaf1208220da04f7"
+};
+  firebase.initializeApp(firebaseConfig);
+  const database = firebase.database();
 
 
-  
-  document.getElementById("submitButton").addEventListener("submit", sendMessage);
+document.getElementById("submitButton").addEventListener("click", sendMessage );
 
-  function sendMessage(e) {
+function sendMessage(e) {
     e.preventDefault();
 
     const timestamp = Date.now();
@@ -40,8 +37,8 @@ const firebaseConfig = {
       message,
     });
   }
-/*
-  const fetchChat = database.ref("messages/");
+
+ const fetchChat = database.ref("messages/");
 
   fetchChat.on("child_added", function (snapshot){
     const messages = snapshot.val();
@@ -51,4 +48,3 @@ const firebaseConfig = {
   // append the message on the page
   document.getElementById("chatBox").innerHTML += message;
   });
-  */
